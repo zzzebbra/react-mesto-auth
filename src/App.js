debc7ae5b07259bb2502d7a32633055a22ea6226
zzzebbra <zzzebbra@yandex.ru>
2020-11-08 17:45:06 +0300

    starting mesto-react

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <body className="app">
+      <header className="App-header">
+
+      </header>
+    </body>
+  );
+}
+
+export default App;
