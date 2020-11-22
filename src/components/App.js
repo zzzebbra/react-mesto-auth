3f814c0dbbc4f91a41bd48cdb02421d568ec4628
zzzebbra <zzzebbra@yandex.ru>
2020-11-22 17:11:06 +0300

    api

--- src/components/App.js
+++ src/components/App.js
@@ -5,7 +5,7 @@ import Main from './Main';
 import Footer from './Footer';
 import PopupWithForm from './PopupWithForm';
 import ImagePopup from './ImagePopup';
-import api from '../utils/api1';
+import api from '../utils/api';
 import CurrentUserContext from '../contexts/CurrentUserContext';
 import EditProfilePopup from './EditProfilePopup';
 import EditAvatarPopup from './EditAvatarPopup';
